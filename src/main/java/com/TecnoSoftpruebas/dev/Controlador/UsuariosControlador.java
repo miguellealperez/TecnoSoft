207d91ddf866d8f6b98d80c178bc2a1377309a37
miguellealperez <mlealp@ucentral.edu.co>
2023-04-13 21:25:32 -0500

    Creación lógica y vista del Login, quedan pendientes del login todavía

--- src/main/java/com/TecnoSoftpruebas/dev/Controlador/UsuariosControlador.java
+++ src/main/java/com/TecnoSoftpruebas/dev/Controlador/UsuariosControlador.java
@@ -6,11 +6,21 @@ package com.TecnoSoftpruebas.dev.Controlador;
 
 import com.TecnoSoftpruebas.dev.Servicios.UsuariosServicio;
 import com.TecnoSoftpruebas.dev.entidades.UsuariosEntidad;
+import jakarta.servlet.http.HttpSession;
 import java.util.List;
+import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.userdetails.User;
 
 /**
  *
@@ -18,17 +28,74 @@ import org.springframework.web.bind.annotation.GetMapping;
  */
 @Controller
 public class UsuariosControlador {
-    private UsuariosServicio usuariosServicio;
-    
+
     @Autowired
-    public UsuariosControlador(UsuariosServicio usuariosServicio){
+    private UsuariosServicio usuariosServicio;
+
+    @Autowired
+    public UsuariosControlador(UsuariosServicio usuariosServicio) {
         this.usuariosServicio = usuariosServicio;
     }
-    
+
     @GetMapping("/usuarios")
-    public String listarUsuarios(Model model){
+    public String listarUsuarios(Model model) {
         List<UsuariosEntidad> usuarios = usuariosServicio.listarUsuarios();
         model.addAttribute("usuarios", usuarios);
         return "usuarios";
+    }
+
+    @GetMapping("/usuarios/nuevo")
+    public String crearUsuario(Model model) {
+        UsuariosEntidad usuario = new UsuariosEntidad();
+        model.addAttribute("usuario", usuario);
+        return "usuarionuevo";
+    }
+
+    @PostMapping("/usuarios/crear")
+    public String guardarUsuario(@ModelAttribute("usuario") UsuariosEntidad usuario, Model model) {
+        try {
+            usuariosServicio.crearUsuario(usuario);
+            return "redirect:/usuarios";
+        } catch (DataIntegrityViolationException e) {
+            model.addAttribute("error", "El usuario ya está registrado.");
+            return "usuarionuevo";
+        } catch (UsuariosServicio.UsuarioExistenteException e) {
+            model.addAttribute("error", "El usuario ya está registrado.");
+            return "usuarionuevo";
+        }
+    }
+
+    @GetMapping("/usuarios/eliminar/{usuarioID}")
+    public String eliminarUsuario(@PathVariable("usuarioID") Long usuarioID) {
+        usuariosServicio.eliminarUsuarioPorId(usuarioID);
+        return "redirect:/usuarios";
+    }
+
+    @GetMapping("/login")
+    public String showLoginForm(Model model) {
+        UsuariosEntidad usuario = new UsuariosEntidad();
+        model.addAttribute("usuario", usuario);
+        return "login";
+    }
+
+    @PostMapping("/login")
+    public String submitLoginForm(@ModelAttribute("usuario") UsuariosEntidad usuario, BindingResult bindingResult, HttpSession session, Model model) {
+        UsuariosEntidad authenticatedUser = usuariosServicio.authenticateUser(usuario.getCorreo(), usuario.getContrasenia());
+        if (authenticatedUser != null) {
+            List<GrantedAuthority> authorities = AuthorityUtils.createAuthorityList("ROLE_USER");
+            User user = new User(authenticatedUser.getCorreo(), authenticatedUser.getContrasenia(), authorities);
+            session.setAttribute("user", user);
+            return "redirect:/usuarios";
+        } else {
+            bindingResult.rejectValue("contrasenia", "error.user", "Contraseña incorrecta");
+            model.addAttribute("loginError", true);
+            return "login";
+        }
+    }
+
+    @GetMapping("/logout")
+    public String logout(HttpSession session) {
+        session.invalidate();
+        return "redirect:/login";
     }
 }
